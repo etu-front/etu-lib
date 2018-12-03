91a697399e62e9adbaef306bef3007a8f49a6821
handylee <lihandi@gmail.com>
2018-12-03 11:02:59 +0800

    feat: 增加 FullScreenModal

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import View from './components/View'
 import FileIcon from './components/FileIcon'
 import MediaList from './components/MediaList'
 import SlideShow from './components/SlideShow'
+import FullScreenModal from './components/FullScreenModal'
 
-export { View, FileIcon, MediaList, SlideShow }
+export { View, FileIcon, MediaList, SlideShow, FullScreenModal }
